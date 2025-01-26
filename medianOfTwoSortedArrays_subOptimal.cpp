45a0ab202f60f0771af27c845edb0942b732df09
deepatidarr <99347948+deepatidarr@users.noreply.github.com>
2025-01-25 22:26:12 -0800

    Update medianOfTwoSortedArrays_subOptimal.cpp

--- medianOfTwoSortedArrays_subOptimal.cpp
+++ medianOfTwoSortedArrays_subOptimal.cpp
@@ -24,3 +24,43 @@ public:
         return ans;
     }
 };
+
+//SPACE OPTIMISED
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        //vector<int> merged;
+        int n = n1+n2;
+        int ind1 = n/2;
+        int ind2 = ind1-1;
+        int i=0; int j=0;
+        int ele1, ele2;
+        int cnt=0;
+        while(i<n1 && j<n2){
+            if(nums1[i] < nums2[j]){
+                if(cnt == ind1) ele1 = nums1[i];
+                if(cnt == ind2) ele2 = nums1[i];
+                cnt++; i++;
+            }
+            else{
+                if(cnt == ind1) ele1 = nums2[j];
+                if(cnt == ind2) ele2 = nums2[j];
+                cnt++; j++;
+            }
+        }
+        while(i<n1){
+            if(cnt == ind1) ele1 = nums1[i];
+            if(cnt == ind2) ele2 = nums1[i];
+            cnt++; i++;
+        }
+        while(j<n2){
+            if(cnt == ind1) ele1 = nums2[j];
+            if(cnt == ind2) ele2 = nums2[j];
+            cnt++; j++;
+        }
+        if(n&1) return ele1;
+        return (ele1+ele2)/2.0;
+    }
+};
